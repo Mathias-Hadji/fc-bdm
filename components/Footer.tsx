9dfc4db4553d5bd0d019fcef6b1a6b549c1a6e6a
Mathias Hadji <mathias.hadji@gmail.com>
2025-03-16 20:25:20 +0100

    fix padding height footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full bg-gray-900 text-white text-center py-4 fixed bottom-0 left-0 ">
+        <footer className="w-full bg-gray-900 text-white text-center py-2 fixed bottom-0 left-0 ">
             <p className="text-sm">
                 Dernière mise à jour du classement: 16 mars 2025
             </p>
